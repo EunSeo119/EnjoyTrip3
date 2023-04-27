6020fa33d491fba93359d595bbf204863d0eb9a1
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-27 17:51:19 +0900

    UPDATE travel

--- EnjoyTrip/src/main/java/com/ssafy/enjoytrip/model/Service/TravelService.java
+++ EnjoyTrip/src/main/java/com/ssafy/enjoytrip/model/Service/TravelService.java
@@ -2,6 +2,7 @@ package com.ssafy.enjoytrip.model.Service;
 
 import com.ssafy.enjoytrip.model.Gugun;
 import com.ssafy.enjoytrip.model.TravelInfo;
+import com.ssafy.enjoytrip.model.TravelStatus;
 
 import java.util.List;
 
@@ -15,4 +16,14 @@ public interface TravelService {
     List<TravelInfo> getTravel(int sidoCode, int gugunCode) throws Exception;
 
     List<TravelInfo> getTravel(int sidoCode, int gugunCode, int travelTypeId) throws Exception;
+
+    int updateLike(String userId, Integer travelInfoId, Integer like);
+
+    TravelStatus getTravelStatus(String userId, Integer travelInfoId);
+
+    void registLike(String userId, int travelInfoId);
+
+    void registStar(TravelStatus travelStatus);
+
+    int updateStar(TravelStatus travelStatus);
 }
